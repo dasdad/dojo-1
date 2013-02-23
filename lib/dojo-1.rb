854528558ea983e81ee5d9d69f3f6d4e184ffc74
Rafael da Silva Almeida <eusou@rafaelalmeida.net>
2013-02-23 01:27:14 -0300

    inclui alguns metodos para identificar somente a multiplicacao de 5 de cada sequencia.

--- lib/dojo-1.rb
+++ lib/dojo-1.rb
@@ -1,50 +1,52 @@
-def teste
-	true
-end
-
-def tabuleiro
-	[[2,1,1,1,1],[2,1,1,1,1],[2,1,1,1,1],[2,1,1,1,1],[2,1,1,1,1]]
-end
-
 def multiplica_coluna(tab, index)
-	mult = 1
-	tab.each do |linha|
-		mult *= linha[index]
-	end
-	mult
+	identifica_maior_multiplicador_com_cinco_numeros( tab.map{|a| a[index] } )
 end
 
 def multiplica_linha(tab, index)
-	mult = 1
-	tab[index].each do |elemento|
-		mult *= elemento
-	end
-	mult
+	identifica_maior_multiplicador_com_cinco_numeros( tab[index] )
 end
 
-def multiplica_diagonal(tab, i, j)
-	mult = 1
-	while i < tab.size && j < tab[0].size
-		mult *= tab[i][j]
-		i+=1
-		j+=1
-	end 
+def multiplica_diagonal(tab)
+	
+	posicoes_para_diagonais = (0..(tab.size-5)).to_a
+	possibilidades = []
+	posicoes_para_diagonais.each do |inicio_x|
+		possibilidade = []
+		posicoes_para_diagonais.each do |inicio_y|
+			
+			i_x =inicio_x
+			i_y =inicio_y
+			while(i_x <= tab.size-1 && i_y <= tab.size-1)
+				possibilidade << tab[i_x][i_y]
+				i_x +=1
+				i_y +=1
+			end
 
-	return mult
+		end
+		possibilidades <<possibilidade
+		
+	end
+	possibilidades.map{ |p| identifica_maior_multiplicador_com_cinco_numeros( p ) }.max
 end
 
 def maior_produto( tabuleiro )
-	maior = 0
-	tabuleiro.each_with_index do | linha, index |
-		produto = multiplica_coluna( tabuleiro, index )
-		maior = produto if produto > maior
-	end
-	tabuleiro.each_with_index do | linha, index |
-		produto = multiplica_linha( tabuleiro, index )
-		maior = produto if produto > maior
-	end
-	diagonal = multiplica_diagonal(tabuleiro,0,0)
-	
-	maior = diagonal if diagonal > maior
+	return 0 if tabuleiro.size < 5 || (tabuleiro.first||[]).size < 5
+	return 0 unless tabuleiro.size == tabuleiro.first.size
+
+	maior = [
+		(0..tabuleiro.size-1).to_a.map{|a| multiplica_coluna( tabuleiro, a ) }.max,
+		(0..tabuleiro.size-1).to_a.map{|a| produto = multiplica_linha( tabuleiro, a ) }.max,
+		multiplica_diagonal(tabuleiro),
+		multiplica_diagonal(tabuleiro.map(&:reverse))
+	].max
+
 	maior
+end
+
+def identifica_maior_multiplicador_com_cinco_numeros( array )
+	possibilidades = (0..array.size).to_a.select{|inicio| inicio+5 <= array.size}.map do | inicio |
+		array[inicio..(inicio+4)].inject(1) {|product, n| product * n }
+	end
+
+	possibilidades.max
 end
--- spec/dojo-1_spec.rb
+++ spec/dojo-1_spec.rb
@@ -1,71 +1,304 @@
 require 'spec_helper'
 
 describe "dojo-1" do
-	it "return true " do
-		teste.should == true
+	describe "tabuleiro de 5x5" do 
+		describe "um tabuleiro pode ter a maior multiplicacao nas colunas" do
+			describe "um tabuleiro com a primeira coluna sendo 2 em todas as posicoes" do
+				before(:each) do
+					@tabuleiro = [
+						[2,1,1,1,1],
+						[2,1,1,1,1],
+						[2,1,1,1,1],
+						[2,1,1,1,1],
+						[2,1,1,1,1]
+					]  
+				end
+				
+				it "multiplica_coluna com index 0" do
+					multiplica_coluna(@tabuleiro, 0).should == 32
+				end
+				it "multiplica_coluna com index 1" do
+					multiplica_coluna(@tabuleiro, 1).should == 1
+				end
+
+				it "maior multiplicador" do
+					maior_produto(@tabuleiro).should == 32
+				end
+			end
+			
+			describe "um tabuleiro com a primeira coluna sendo 2 em todas as posicoes, menos na ultima" do
+				before(:each) do
+					@tabuleiro = [
+						[2,1,1,1,1],
+						[2,1,1,1,1],
+						[2,1,1,1,1],
+						[2,1,1,1,1],
+						[1,2,1,1,1]
+					]
+				end
+				
+				it "multiplica_coluna com index 0" do
+					multiplica_coluna(@tabuleiro, 0).should == 16
+				end
+
+				it "multiplica_coluna com index 1" do
+					multiplica_coluna(@tabuleiro, 1).should == 2
+				end
+
+				it "maior multiplicador" do
+					maior_produto(@tabuleiro).should == 16
+				end
+
+			end		
+		end
+
+		describe "um tabuleiro com a segunda coluna sendo 2 em todas as posicoes, menos na ultima" do
+			before(:each) do
+				@tabuleiro = [
+					[1,2,1,1,1],
+					[1,2,1,1,1],
+					[1,2,1,1,1],
+					[1,2,1,1,1],
+					[2,1,1,1,1]
+				]
+			end
+
+			it "multiplica_coluna com index 0" do
+				multiplica_coluna(@tabuleiro, 0).should == 2
+			end
+			it "multiplica_coluna com index 1" do
+				multiplica_coluna(@tabuleiro, 1).should == 16
+			end
+			it "maior multiplicador" do
+				maior_produto(@tabuleiro).should == 16
+			end
+
+		end		
+
+
+		describe "um tabuleiro com a primeira linha sendo 2 em todas as posicoes, menos na ultima" do
+			before(:each) do
+				@tabuleiro = [
+					[2,2,2,2,1],
+					[1,1,1,1,1],
+					[1,1,1,1,1],
+					[1,1,1,1,1],
+					[1,1,1,1,1]
+				]
+			end
+
+			it "multiplica_linha com index 0" do
+				multiplica_linha(@tabuleiro, 0).should == 16
+			end
+
+			it "multiplica_linha com index 1" do
+				multiplica_linha(@tabuleiro, 1).should == 1
+			end
+
+			it "maior multiplicador linha" do
+				maior_produto(@tabuleiro).should == 16
+			end
+		end
+
+		describe "um tabuleiro com a primeira maior multiplicacao sendo nas diagoanis" do
+			it "maior multiplicador diagonal" do
+				tabuleiro = [
+					[2,1,1,1,1],
+					[1,2,1,1,1],
+					[1,1,2,1,1],
+					[1,1,1,2,1],
+					[1,1,1,1,2]
+				]
+				maior_produto(tabuleiro).should == 32
+			end
+
+
+			it "maior multiplicador diagonal " do
+				tabuleiro = [
+					[1,1,1,1,2],
+					[1,1,1,2,1],
+					[1,1,2,1,1],
+					[1,2,1,1,1],
+					[2,1,1,1,1]
+				]
+
+				maior_produto(tabuleiro).should == 32
+			end
+		end
 	end
 
-	it "valida tabuleiro" do
-		tabuleiro.should == [
-			[2,1,1,1,1],
-			[2,1,1,1,1],
-			[2,1,1,1,1],
-			[2,1,1,1,1],
-			[2,1,1,1,1]
-		]
+	describe "tabuleiro maior do que 5x5" do 
+		describe "um tabuleiro pode ter a maior multiplicacao nas colunas" do
+			describe "um tabuleiro com a primeira coluna sendo 2 em todas as posicoes" do
+				before(:each) do
+					@tabuleiro = [
+						[2,1,1,1,1,1],
+						[2,1,1,1,1,1],
+						[2,1,1,1,1,1],
+						[2,1,1,1,1,1],
+						[2,1,1,1,1,1],
+						[2,1,1,1,1,1]
+					]  
+				end
+				
+				it "multiplica_coluna com index 0" do
+					multiplica_coluna(@tabuleiro, 0).should == 32
+				end
+				it "multiplica_coluna com index 1" do
+					multiplica_coluna(@tabuleiro, 1).should == 1
+				end
+
+				it "maior multiplicador" do
+					maior_produto(@tabuleiro).should == 32
+				end
+			end
+			
+			describe "um tabuleiro com a primeira coluna sendo 2 em todas as posicoes, menos na ultima" do
+				before(:each) do
+					@tabuleiro = [
+						[2,1,1,1,1,1],
+						[2,1,1,1,1,1],
+						[2,1,1,1,1,1],
+						[2,1,1,1,1,1],
+						[2,1,1,1,1,1],
+						[1,2,1,1,1,1]
+					]
+				end
+				
+				it "multiplica_coluna com index 0" do
+					multiplica_coluna(@tabuleiro, 0).should == 32
+				end
+
+				it "multiplica_coluna com index 1" do
+					multiplica_coluna(@tabuleiro, 1).should == 2
+				end
+
+				it "maior multiplicador" do
+					maior_produto(@tabuleiro).should == 32
+				end
+
+			end		
+		end
+
+		describe "um tabuleiro com a segunda coluna sendo 2 em todas as posicoes, menos na ultima" do
+			before(:each) do
+				@tabuleiro = [
+					[1,2,1,1,1,1],
+					[1,2,1,1,1,1],
+					[1,2,1,1,1,1],
+					[1,2,1,1,1,1],
+					[1,2,1,1,1,1],
+					[2,1,1,1,1,1]
+				]
+			end
+
+			it "multiplica_coluna com index 0" do
+				multiplica_coluna(@tabuleiro, 0).should == 2
+			end
+			it "multiplica_coluna com index 1" do
+				multiplica_coluna(@tabuleiro, 1).should == 32
+			end
+			it "maior multiplicador" do
+				maior_produto(@tabuleiro).should == 32
+			end
+
+		end		
+
+
+		describe "um tabuleiro com a primeira linha sendo 2 em todas as posicoes, menos na ultima" do
+			before(:each) do
+				@tabuleiro = [
+					[2,2,2,2,2,1],
+					[1,1,1,1,1,1],
+					[1,1,1,1,1,1],
+					[1,1,1,1,1,1],
+					[1,1,1,1,1,1],
+					[1,1,1,1,1,1]
+				]
+			end
+
+			it "multiplica_linha com index 0" do
+				multiplica_linha(@tabuleiro, 0).should == 32
+			end
+
+			it "multiplica_linha com index 1" do
+				multiplica_linha(@tabuleiro, 1).should == 1
+			end
+
+			it "maior multiplicador linha" do
+				maior_produto(@tabuleiro).should == 32
+			end
+		end
+
+		describe "um tabuleiro com a primeira maior multiplicacao sendo nas diagoanis" do
+			it "maior multiplicador diagonal" do
+				tabuleiro = [
+					[2,1,1,1,1,1],
+					[1,2,1,1,1,1],
+					[1,1,2,1,1,1],
+					[1,1,1,2,1,1],
+					[1,1,1,1,2,1],
+					[1,1,1,1,1,2]
+				]
+				maior_produto(tabuleiro).should == 32
+			end
+
+
+			it "maior multiplicador diagonal " do
+				tabuleiro = [
+					[1, 1, 1, 1, 1, 2],
+					[1, 1, 1, 1, 2, 1], 
+					[1, 1, 1, 2, 1, 1], 
+					[1, 1, 2, 1, 1, 1], 
+					[1, 2, 1, 1, 1, 1], 
+					[2, 1, 1, 1, 1, 1]
+				]
+
+				maior_produto(tabuleiro).should == 32
+			end
+
+			it "maior multiplicador diagonal " do
+				tabuleiro = [
+					[1, 1, 1, 1, 1, 1],
+					[1, 1, 1, 1, 1, 2], 
+					[1, 1, 1, 1, 2, 1], 
+					[1, 1, 1, 2, 1, 1], 
+					[1, 1, 2, 1, 1, 1], 
+					[1, 3, 1, 1, 1, 1]
+				]
+
+				maior_produto(tabuleiro).should == 48
+			end
+
+			it "maior multiplicador diagonal " do
+				tabuleiro = [
+					[1, 1, 1, 1, 2, 1],
+					[1, 1, 1, 2, 1, 1], 
+					[1, 1, 2, 1, 1, 1], 
+					[1, 3, 1, 1, 1, 1], 
+					[3, 1, 1, 1, 1, 1], 
+					[1, 1, 1, 1, 1, 1]
+				]
+
+				maior_produto(tabuleiro).should == 72
+			end
+		end
 	end
 
-	it "multiplica colunas" do
-		multiplica_coluna(tabuleiro, 0).should == 32
+	it "identifica_maior_multiplicador_com_cinco_numeros" do
+		identifica_maior_multiplicador_com_cinco_numeros([1,2,3,4,5]).should == 120
 	end
 
-	it "maior multiplicador" do
-	  maior_produto(tabuleiro).should == 32
+	it "identifica_maior_multiplicador_com_cinco_numeros" do
+		identifica_maior_multiplicador_com_cinco_numeros([1,2,3,4,5,6]).should == 720
 	end
 
-	it "maior multiplicador maroto" do
-		tabuleiro = [[2,1,1,1,1],[2,1,1,1,1],[2,1,1,1,1],[2,1,1,1,1],[1,2,1,1,1]]
-	  	maior_produto(tabuleiro).should == 16
+	it "identifica_maior_multiplicador_com_cinco_numeros" do
+		identifica_maior_multiplicador_com_cinco_numeros([1,2,3,4,5,6,7]).should == 2520
 	end
 
-	it "maior multiplicador segunda coluna" do
-		tabuleiro = [[1,2,1,1,1],[1,2,1,1,1],[1,2,1,1,1],[1,2,1,1,1],[1,2,1,1,1]]
-		
-	  	maior_produto(tabuleiro).should == 32
-	end
-
-	it "maior multiplicador linha" do
-		tabuleiro = [[2,2,2,2,2],[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1],[1,1,1,1,1]]
-		maior_produto(tabuleiro).should == 32	  
-	end
-
-	it "maior multiplicador linha valor 3" do
-		tabuleiro = [[3,2,2,2,2],
-					 [1,1,1,1,1],
-					 [1,1,1,1,1],
-					 [1,1,1,1,1],
-					 [1,1,1,1,1]]
-		maior_produto(tabuleiro).should == 48	  
-	end
-
-	it "maior multiplicador diagonal" do
-	 	tabuleiro = [[2,1,1,1,1],
-					 [1,2,1,1,1],
-					 [1,1,2,1,1],
-					 [1,1,1,2,1],
-					 [1,1,1,1,2]]
-		maior_produto(tabuleiro).should == 32
-	end
-
-
-it "maior multiplicador diagonal" do
-	 	tabuleiro = [[2,1,1,1,1],
-					 [1,2,1,1,1],
-					 [1,1,2,1,1],
-					 [1,1,1,2,1],
-					 [1,1,1,1,2]]
-	 	 tabuleiro = tabuleiro.map(&:reverse)
-	 	 maior_produto(tabuleiro).should == 32
+	it "identifica_maior_multiplicador_com_cinco_numeros" do
+		identifica_maior_multiplicador_com_cinco_numeros([2,1,3,2,2,2,2,1,1]).should == 48
 	end
 
 end
